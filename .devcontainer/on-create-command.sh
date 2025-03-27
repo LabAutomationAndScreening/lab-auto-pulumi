6f8cffce87b2b3a68556b3c08b3b958a975fe81b
Eli Fine <eli88fine@gmail.com>
2025-03-27 16:45:42 +0000

    copier init

---
+++ .devcontainer/on-create-command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+# For some reason the directory is not setup correctly and causes build of devcontainer to fail since
+# it doesn't have access to the workspace directory. This can normally be done in post-start-command
+git config --global --add safe.directory /workspaces/lab-auto-pulumi
+
+sh .devcontainer/on-create-command-boilerplate.sh
+
+pre-commit install --install-hooks
+
+sh .devcontainer/manual-setup-deps.sh --optionally-lock
