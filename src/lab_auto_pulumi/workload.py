11defc8bf0e57199345f8d6a5b95c66043cf54fc
Eli Fine <eli88fine@gmail.com>
2025-06-18 14:28:09 +0000

    pyright igoner

--- src/lab_auto_pulumi/workload.py
+++ src/lab_auto_pulumi/workload.py
@@ -7,12 +7,12 @@ from .permissions import AwsAccountInfo
 class AwsLogicalWorkload(BaseModel):
     version: str = "0.0.1"
     name: str
-    prod_accounts: list[AwsAccountInfo] = Field(  # type: ignore[reportUnknownVariableType] # some bug in pyright around 1.1.400 is causing default_factory=list to be unknown
+    prod_accounts: list[AwsAccountInfo] = Field(  # pyright: ignore[reportUnknownVariableType] # some bug in pyright around 1.1.400 is causing default_factory=list to be unknown
         default_factory=list
     )  # TODO: convert to a set with deterministic ordering to avoid false positive diffs
-    staging_accounts: list[AwsAccountInfo] = Field(  # type: ignore[reportUnknownVariableType] # some bug in pyright around 1.1.400 is causing default_factory=list to be unknown
+    staging_accounts: list[AwsAccountInfo] = Field(  # pyright: ignore[reportUnknownVariableType] # some bug in pyright around 1.1.400 is causing default_factory=list to be unknown
         default_factory=list
     )  # TODO: convert to a set with deterministic ordering to avoid false positive diffs
-    dev_accounts: list[AwsAccountInfo] = Field(  # type: ignore[reportUnknownVariableType] # some bug in pyright around 1.1.400 is causing default_factory=list to be unknown
+    dev_accounts: list[AwsAccountInfo] = Field(  # pyright: ignore[reportUnknownVariableType] # some bug in pyright around 1.1.400 is causing default_factory=list to be unknown
         default_factory=list
     )  # TODO: convert to a set with deterministic ordering to avoid false positive diffs
